6bd49bee9d34a1cae8f49255e85569a56952f65d
marie-rousselot <159030042+marie-rousselot@users.noreply.github.com>
2024-04-26 20:47:11 +0800

    added basic javascript and typewriter plug in

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function generateFortune(event) {
+  event.preventDefault();
+
+  new Typewriter("#fortune", {
+    strings: ["Dear Gemini..."],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let fortuneFormElement = document.querySelector("#funny-fortune-generator");
+fortuneFormElement.addEventListener("submit", generateFortune);
