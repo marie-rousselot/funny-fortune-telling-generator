c65d65fb301f382bc18023939380c650a0657ad6
marie-rousselot <159030042+marie-rousselot@users.noreply.github.com>
2024-04-27 21:54:26 +0800

    improved AI context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateFortune(event) {
   let apiKey = "fdo0da8da8fd60t560b33ec66f43f0c4";
   let prompt = `Generate a funny fortune telling related to ${instructionsInput.value}`;
   let context =
-    "You are a funny fortune teller. Your mission is to generate a funny prediction according to my star sign. Be respectful and polite. Your answer must be short and fun and should include 3 different topics separated with a <br />: love, wealth and coding skills. Make sure to use the user instructions. Sign the fortune telling with 'Shecodes AI'in a <strong> at the end of the fortune telling and separate the signature with a <br />";
+    "You are a funny fortune teller. Your mission is to generate a funny prediction according to my star sign. Be respectful and polite. Your answer must be short and fun and should include 3 different topics separated with a <br />: love, wealth and coding skills. Make sure to use the user instructions. Sign the fortune telling with 'Shecodes AI'in a <strong> at the end of the fortune telling and separate the signature with a <br />. If value entered is not a star sign, display 'Hem...sorry, I said enter YOUR STAR SIGN, try again 🤔";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let fortuneElement = document.querySelector("#fortune");
